ca8887b453a3404afddd0b2501590ccaa88ad5a3
sibmurzik <60308885+sibmurzik@users.noreply.github.com>
2024-10-01 21:16:01 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+#Практическое задание по работе в Pycharm - "Переменные".
+
+home_task_quantity = 12
+spent_time = 1.5
+course_mane = 'Python'
+time_for_1_task = spent_time/home_task_quantity
+
+print("Курс: " , course_mane, " всего задач: ", home_task_quantity,
+      " затрачено часов: " , spent_time, " среднее время выполнения ",  time_for_1_task, " часа.")
