501f013b5e6ad73ce8148ec86fa844ff0a4bedd7
sibmurzik <60308885+sibmurzik@users.noreply.github.com>
2024-10-01 16:49:40 +0700

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+#Практическая работа по уроку "Динамическая типизация"
+
+name = "Андрей"
+print("Name: ", name)
+age = 50
+print("Age: ", age)
+age = age+1
+print("Newe age: ",age)
+is_student = True
+print("Is student: ", is_student)
