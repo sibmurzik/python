2e457774756c67d4cb4a461b91d31bfbd0e0d5c2
sibmurzik <60308885+sibmurzik@users.noreply.github.com>
2024-10-08 21:23:18 +0700

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,12 @@
+#"Условная конструкция. Операторы if, elif, else"
+
+first = input("Введите первое число: ")
+second = input("Введите второе число: ")
+third = input ("Введите третье число: ")
+
+if first == second == third:
+    print( 3 )
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
