a1e87e2086e420a91fab46c22ea79fa1cb9d1837
sibmurzik <60308885+sibmurzik@users.noreply.github.com>
2024-10-17 21:49:08 +0700

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,28 @@
+"Способы вызова функции"
+
+def send_email(message, recipient, *,  sender = "university.help@gmail.com"):
+    if not check_is_adress_correct(str(recipient)) or not check_is_adress_correct(str(sender)):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+        return
+    if recipient == sender:
+        print("Нельзя отправить письмо самому себе!")
+        return
+    if sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+        return
+    print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+
+
+
+
+def check_is_adress_correct( adress: str):
+    if "@" in adress:
+        if adress.endswith( ".com") or adress.endswith(".ru") or adress.endswith(".net"):
+            return True
+    return False
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
