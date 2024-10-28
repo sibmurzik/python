d0d2f1c3a114770b38d58f148a8647be1f2b7d38
sibmurzik <60308885+sibmurzik@users.noreply.github.com>
2024-10-28 11:12:37 +0700

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -3,6 +3,8 @@
 def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
+    if first == 0:
+        return 1  
     if len(str_number) > 1:
         #print(first , ' * get_multiplied_digits(', int(str_number[1:]), ')', )
         return first * get_multiplied_digits(int(str_number[1:]))
