0df676a629edfd58d33586951a2dcf5deb919ce3
sibmurzik <60308885+sibmurzik@users.noreply.github.com>
2024-11-11 06:53:06 +0700

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,96 @@
+# "Перегрузка операторов"
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        floor = int(new_floor)
+        if floor < 1 or floor > self.number_of_floors:
+            print("Такого этажа не существует")
+        else:
+            for i in range(1, floor + 1):
+                print(i)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}."
+
+    def __eq__(self, other):
+        if self.checkIfBothHouses(other):
+            return self.number_of_floors == other.number_of_floors
+        else:
+            return False
+
+    def __lt__(self, other):
+        if self.checkIfBothHouses(other):
+            return self.number_of_floors < other.number_of_floors
+        else:
+            return False
+
+    def __le__(self, other):
+        if self.checkIfBothHouses(other):
+            return self.number_of_floors <= other.number_of_floors
+        else:
+            return False
+
+    def __gt__(self, other):
+        if self.checkIfBothHouses(other):
+            return self.number_of_floors > other.number_of_floors
+        else:
+            return False
+
+    def __ge__(self, other):
+        if self.checkIfBothHouses(other):
+         return self.number_of_floors <= other.number_of_floors
+        else:
+            return False
+
+    def __ne__(self, other):
+        if self.checkIfBothHouses(other):
+            return self.number_of_floors != other.number_of_floors
+        else:
+            return False
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+        else:
+            return NotImplemented
+        return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+    def checkIfBothHouses(self, obj2):
+        return isinstance(self, House) and isinstance(obj2, House)
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+print(h1)
+print(h2)
+
+print(h1 == h2)  # __eq__
+h1 = h1 + 10  # __add__
+print(h1)
+print(h1 == h2)
+print(h1 == 10)
+h1 += 10  # __iadd__
+print(h1)
+h2 = 10 + h2  # __radd__
+print(h2)
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+
+print(h1 != h2)  # __ne__
+
+h1 = h1 + h2
+print(h1)
