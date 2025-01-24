e665959973dca57747cc17355c3ad3590c6d4b09
sibmurzik <60308885+sibmurzik@users.noreply.github.com>
2025-01-24 12:05:32 +0100

    Add files via upload

---
+++ mldule 13_4.py
@@ -0,0 +1,58 @@
+#"Цепочка вопросов"
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+
+api =""
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage= MemoryStorage())
+
+class UserState (StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+@dp.message_handler(text = 'Calories')
+async def set_age(message: types.Message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    await state.update_data(age = message.text)
+    await message.answer("Введите свой рост: ")
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message : types.Message, state: FSMContext):
+    await state.update_data( growth = message.text)
+    await message.answer("Введите свой вес: " )
+    await UserState.weight.set()
+
+@dp.message_handler(state= UserState.weight)
+async def send_calories(message: types.Message, state : FSMContext):
+    await state.update_data(weight = message.text)
+    data = await state.get_data()
+    try:
+        age = int(data['age'])
+        growth = int(data['growth'])
+        weight = int(data['weight'])
+    except ValueError as e:
+        await message.answer(f"You have sent incorrect date {e}")
+    else:
+        calories = 10 * weight + 6.25 * growth - 5 * age + 5
+        await message.answer(f"Your optimal daily calories is: {calories}")
+    finally:
+        await state.finish()
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates= True)
+
+
+
+
+
+
